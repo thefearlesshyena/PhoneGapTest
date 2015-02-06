0b245ce15ed122418c9e2a2bc1236b2e07a9be24
thefearlesshyena <luke@epimedia.com>
2015-02-06 14:18:04 -0700

    Moved notification code to html file.

--- demo_workers.js
+++ demo_workers.js
@@ -2,16 +2,12 @@ var i = 0;
 
 var msInterval = 500;
 
-//notification plugin
-document.addEventListener('deviceready', function () {
-    // window.plugin.notification.local is now available
-}, false);
+
 
 function setWorkerInterval(val){
 
 	msInterval = val;
-//added the following line to create a notification when user presses worker interval.	
-window.plugin.notification.local.add({ message: 'Great app!' });
+
 }
 
 function timedCount() {

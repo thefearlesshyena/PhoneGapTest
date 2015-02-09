b7e7124afd892b2c2ce0e11a82e5c8ea3a01a74d
thefearlesshyena <luke@epimedia.com>
2015-02-09 12:04:51 -0700

    Moved notification to javascript doc.

--- demo_workers.js
+++ demo_workers.js
@@ -14,6 +14,8 @@ function timedCount() {
     i = i + 1;
     postMessage(i);
     setTimeout("timedCount()",msInterval);
+	//added the following line to create a notification when user presses worker interval.	
+window.plugin.notification.local.add({ message: 'Great app!' });
 }
 
 timedCount(); 
